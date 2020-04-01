f76985bc4fe936a79733465fee1fe1826a33f59d
Sandeep Chandran <sandeep@paypossible.com>
2020-04-01 17:09:00 -0500

    test

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,8 @@
 // const axios = require("axios")
 
 exports.handler = async (event, context) => {
-  // const parsedBody = JSON.parse(event.body)
+  const parsedBody = JSON.parse(event.body)
+  console.log(parsedBody)
   try {
     // const response = await axios({
     //   method: "post",
@@ -12,10 +13,10 @@ exports.handler = async (event, context) => {
     // const data = await response.json()
     return {
       statusCode: 200,
-      body: `Hello!`,
+      body: parsedBody,
     }
   } catch (error) {
     console.log(error)
-    return new Error("something went wrong")
+    return new Error("something went wrong", error)
   }
 }
