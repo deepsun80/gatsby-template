3ba231b6d114bde8d2d5583cf35557d5a51e8e30
Sandeep Chandran <sandeep@paypossible.com>
2020-04-02 08:24:54 -0500

    test

--- functions/test.js
+++ functions/test.js
@@ -1,21 +1,50 @@
-// const axios = require("axios")
+const axios = require("axios")
 
-exports.handler = async (event, context) => {
-  // const parsedBody = JSON.parse(event.body)
-  try {
-    // const response = await axios({
-    //   method: "post",
-    //   url: "http://requestbin.net/r/wppv6lwp",
-    //   data: parsedBody,
-    // })
+// exports.handler = async (event, context) => {
+//   const parsedBody = JSON.parse(event.body)
+//   try {
+//     // const response = await axios({
+//     //   method: "post",
+//     //   url: "http://requestbin.net/r/wppv6lwp",
+//     //   data: parsedBody,
+//     // })
 
-    // const data = await response.json()
-    return {
-      statusCode: 200,
-      body: event.body,
-    }
-  } catch (error) {
-    console.log(error)
-    return new Error("something went wrong")
-  }
+//     // const data = await response.json()
+//     // alert(parsedBody)
+//     return {
+//       statusCode: 200,
+//       body: parsedBody,
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return new Error("something went wrong")
+//   }
+// }
+
+exports.handler = (event, context) => {
+  const parsedBody = JSON.parse(event.body)
+  axios({
+    method: "post",
+    url: "http://requestbin.net/r/1f81yr81",
+    data: parsedBody,
+  })
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          statusCode: 200,
+          body: result,
+        }),
+      }
+      callback(null, response)
+    })
+    .catch(err => {
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: err.message,
+        }),
+      }
+      callback(null, response)
+    })
 }
